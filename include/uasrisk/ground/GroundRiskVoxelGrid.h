694b3a4db12d97e974125da8138d1c4d2a0de68a
Aliaksei Pilko <aliakseipilko1@gmail.com>
2022-04-04 16:34:31 +0100

    Fix out of scope impl

--- include/uasrisk/ground/GroundRiskVoxelGrid.h
+++ include/uasrisk/ground/GroundRiskVoxelGrid.h
@@ -6,33 +6,31 @@
 
 namespace ur
 {
-    class GroundRiskVoxelGrid : public VoxelGrid
-    {
-    public:
-        GroundRiskVoxelGrid(const std::array<FPScalar, 6>& bounds, FPScalar xyRes,
-                            FPScalar zRes,
-                            const char* worldSrs, ugr::mapping::PopulationMap* populationMap,
-                            ugr::risk::AircraftModel* aircraftModel,
-                            ugr::risk::ObstacleMap* obstacleMap,
-                            ugr::risk::WeatherMap* weather);
+	class GroundRiskVoxelGrid : public VoxelGrid
+	{
+	public:
+		GroundRiskVoxelGrid(const std::array<FPScalar, 6>& bounds, FPScalar xyRes,
+		                    FPScalar zRes,
+		                    const char* worldSrs, ugr::mapping::PopulationMap* populationMap,
+		                    ugr::risk::AircraftModel* aircraftModel,
+		                    ugr::risk::ObstacleMap* obstacleMap,
+		                    ugr::risk::WeatherMap* weather);
 
-        ~GroundRiskVoxelGrid();
+		~GroundRiskVoxelGrid()
+		{
+			delete populationMap;
+			delete aircraftModel;
+			delete obstacleMap;
+			delete weatherMap;
+		}
 
-        void eval();
-    protected:
-        std::map<double, ugr::risk::RiskMap> altitudeRiskMap;
-        ugr::mapping::PopulationMap* populationMap;
-        ugr::risk::AircraftModel* aircraftModel;
-        ugr::risk::ObstacleMap* obstacleMap;
-        const ugr::risk::WeatherMap* weatherMap;
-    };
-
-    inline GroundRiskVoxelGrid::~GroundRiskVoxelGrid()
-    {
-        delete populationMap;
-        delete aircraftModel;
-        delete obstacleMap;
-        delete weatherMap;
-    }
+		void eval();
+	protected:
+		std::map<double, ugr::risk::RiskMap> altitudeRiskMap;
+		ugr::mapping::PopulationMap* populationMap;
+		ugr::risk::AircraftModel* aircraftModel;
+		ugr::risk::ObstacleMap* obstacleMap;
+		const ugr::risk::WeatherMap* weatherMap;
+	};
 }
 #endif // UR_GROUNDRISKVOXELGRID_H
