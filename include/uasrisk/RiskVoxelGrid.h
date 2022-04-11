f255385c22ee4ce4c3416c148a62f8401651caa3
Aliaksei Pilko <A.Pilko@soton.ac.uk>
2022-04-11 14:32:22 +0100

    Add combined risk voxel grid

---
+++ include/uasrisk/RiskVoxelGrid.h
@@ -0,0 +1,24 @@
+#ifndef UR_RISKVOXELGRID_H
+#define UR_RISKVOXELGRID_H
+#include <uasrisk/air/AirRiskVoxelGrid.h>
+#include <uasrisk/ground/GroundRiskVoxelGrid.h>
+#include <uasrisk/environment/VoxelGrid.h>
+
+namespace ur
+{
+	class RiskVoxelGrid : public VoxelGrid
+	{
+	public:
+		RiskVoxelGrid(const std::array<FPScalar, 6>& bounds, const FPScalar xyRes, const FPScalar zRes,
+		              const std::string& trajPath, const std::string& airspacePath,
+		              ugr::mapping::PopulationMap* const populationMap, ugr::risk::AircraftModel* const aircraftModel,
+		              ugr::risk::ObstacleMap* const obstacleMap, ugr::risk::WeatherMap* const weather);
+
+		void eval();
+
+	protected:
+		AirRiskVoxelGrid arvg;
+		GroundRiskVoxelGrid grvg;
+	};
+}
+#endif // UR_RISKVOXELGRID_H
