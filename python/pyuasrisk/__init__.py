8c676a80a5a49de4f6c166779c87220aae21ff39
Aliaksei Pilko <a.pilko@soton.ac.uk>
2022-08-31 14:39:55 +0100

    Clean out unnecessary python package code

--- python/pyuasrisk/__init__.py
+++
@@ -1,3 +0,0 @@
-from .pyuasrisk import VoxelGrid, __doc__, GroundRiskVoxelGrid, AirRiskVoxelGrid, RiskVoxelGrid, AircraftModel, AircraftStateModel, PopulationMap, TemporalPopulationMap
-
-__all__ = (VoxelGrid, GroundRiskVoxelGrid, AirRiskVoxelGrid, RiskVoxelGrid, AircraftModel, AircraftStateModel, PopulationMap, TemporalPopulationMap)
--- python/pyuasrisk/test.py
+++
@@ -1,8 +0,0 @@
-import os
-import sys
-import subprocess
-
-# Build the python module
-curdir = os.path.abspath(os.path.dirname(__file__))
-subprocess.check_call([sys.executable, os.path.join(curdir, 'setup.py'), 'build_ext', '--inplace'], cwd=curdir)
-
