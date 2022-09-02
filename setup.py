7503ac3e78ec7917d6a2cdd1fb5d447aa4e206b6
Aliaksei Pilko <a.pilko@soton.ac.uk>
2022-09-02 11:09:23 +0100

    Add data package

--- setup.py
+++ setup.py
@@ -2,22 +2,24 @@ import sys
 import os
 import shutil
 import sysconfig
+import platform
 
 from skbuild import setup
 from setuptools import find_packages
+
 
 # Remove previous build folder
 def remove_readonly(func, path, excinfo):
     os.chmod(path, stat.S_IWRITE)
     os.remove(path)
 
+
 if os.path.exists('_skbuild'):
     shutil.rmtree('_skbuild', onerror=remove_readonly)
 
 # The information here can also be placed in setup.cfg - better separation of
 # logic and declaration, and simpler if you include description/version in a file.
-package_data_dir= sysconfig.get_paths()['purelib'] + '/pyuasrisk/data'
-print(package_data_dir)
+package_data_dir = sysconfig.get_paths()['purelib'] + '/pyuasrisk/data'
 setup(
     name="pyuasrisk",
     version="0.0.1",
@@ -26,11 +28,11 @@ setup(
     description="Risk calculation for uncrewed aerial systems",
     long_description="",
     license="Proprietary",
-    packages=['pyuasrisk'],
+    packages=['pyuasrisk', 'pyuasrisk.data'],
     cmake_install_dir="python/pyuasrisk",
     cmake_args=[f"-DUGR_DATA_DIR={package_data_dir}"],
     include_package_data=True,
-    package_data={"pyuasrisk": ["data/*.*"]},
+    package_data={"pyuasrisk": ["data/*.*", ]},
     package_dir={"": "python"},
     python_requires=">=3.6",
 )
