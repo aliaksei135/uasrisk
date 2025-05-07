d1cb2582e38c9e1a4cb0c23e9848b70462082367
Aliaksei Pilko <a.pilko@soton.ac.uk>
2025-05-07 09:38:48 +0100

    Use scikit-build-core for bindings

--- python/pyuasrisk/__init__.py
+++ python/pyuasrisk/__init__.py
@@ -1,21 +1,35 @@
-from ctypes import cdll
-import os
-from pathlib import Path
+from ._pyuasrisk import (
+    __version__,
+    __doc__,
+    VoxelGrid,
+    GridMap,
+    GeospatialGridMap,
+    AircraftStateModel,
+    AircraftModel,
+    PopulationMap,
+    TemporalPopulationMap,
+    ObstacleMap,
+    WeatherMap,
+    FullGroundRiskVoxelGrid,
+    GroundRiskVoxelGrid,
+    IncrementalGroundRiskVoxelGrid,
+    AirRiskVoxelGrid,
+    RiskVoxelGrid,
+)
 
-current_dir = Path(os.path.dirname(os.path.realpath(__file__)))
-
-if os.path.exists(current_dir / 'lib'):
-    lib_dir = current_dir / 'lib'
-elif os.path.exists(current_dir / 'lib64'):
-    lib_dir = current_dir / 'lib64'
-
-cdll.LoadLibrary(lib_dir / 'libuasgroundrisk.so')
-cdll.LoadLibrary(lib_dir / 'libuasrisk.so')
-
-from ._pyuasrisk import __version__, __doc__, VoxelGrid, GridMap, GeospatialGridMap, AircraftStateModel, AircraftModel, \
-    PopulationMap, TemporalPopulationMap, ObstacleMap, WeatherMap, FullGroundRiskVoxelGrid, GroundRiskVoxelGrid, \
-    IncrementalGroundRiskVoxelGrid, AirRiskVoxelGrid, RiskVoxelGrid
-
-__all__ = ["VoxelGrid", "GridMap", "GeospatialGridMap", "AircraftStateModel", "AircraftModel", "PopulationMap",
-           "TemporalPopulationMap", "ObstacleMap", "WeatherMap", "FullGroundRiskVoxelGrid", "GroundRiskVoxelGrid",
-           "IncrementalGroundRiskVoxelGrid", "AirRiskVoxelGrid", "RiskVoxelGrid"]
+__all__ = [
+    "VoxelGrid",
+    "GridMap",
+    "GeospatialGridMap",
+    "AircraftStateModel",
+    "AircraftModel",
+    "PopulationMap",
+    "TemporalPopulationMap",
+    "ObstacleMap",
+    "WeatherMap",
+    "FullGroundRiskVoxelGrid",
+    "GroundRiskVoxelGrid",
+    "IncrementalGroundRiskVoxelGrid",
+    "AirRiskVoxelGrid",
+    "RiskVoxelGrid",
+]
